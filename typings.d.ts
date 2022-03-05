9ed72ca4ef05cd9bfc764c41b5d9434b3cb0e784
Luigi Pizarro <soyluking@gmail.com>
2022-03-05 13:45:42 -0300

    feat: add comment type to typings

--- typings.d.ts
+++ typings.d.ts
@@ -11,9 +11,26 @@ export interface Post {
       url: string;
     };
   };
+  comments: Comment[];
   body?: [object];
   author: {
     name: string;
     image: string;
   };
+}
+
+export interface Comment {
+  approved: boolean;
+  comment: string;
+  name: string;
+  email: string;
+  post: {
+    _ref: string;
+    _type: string;
+  };
+  _id: string;
+  _createdAt: string;
+  _rev: string;
+  _type: string;
+  _updatedAt: string;
 }
